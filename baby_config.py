2c22484f76e52b4242207543b58950098dbaaed6
Sehwan Yoo <krstyle03v@gmail.com>
2022-10-13 10:46:54 +0900

    .

---
+++ baby_config.py
@@ -0,0 +1,51 @@
+from mmcv import Config
+cfg = Config.fromfile(
+    './configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/coco/hrnet_w32_coco_256x192.py'
+)
+
+cfg.channel_cfg.num_output_channels = 21
+cfg.channel_cfg.dataset_joints = 21
+cfg.channel_cfg.dataset_channel = [ [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]]
+cfg.channel_cfg.inference_channel = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21 ]
+
+# set basic configs
+cfg.data_root = 'data/coco_tiny'
+cfg.work_dir = 'work_dirs/hrnet_w32_coco_tiny_256x192'
+cfg.gpu_ids = range(1)
+cfg.seed = 0
+
+# set log interval
+cfg.log_config.interval = 1
+
+# set evaluation configs
+cfg.evaluation.interval = 10
+cfg.evaluation.metric = 'PCK'
+cfg.evaluation.save_best = 'PCK'
+
+# set learning rate policy
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=10,
+    warmup_ratio=0.001,
+    step=[17, 35])
+cfg.total_epochs = 40
+
+# set batch size
+cfg.data.samples_per_gpu = 16
+cfg.data.val_dataloader = dict(samples_per_gpu=16)
+cfg.data.test_dataloader = dict(samples_per_gpu=16)
+
+
+# set dataset configs
+cfg.data.train.type = 'TopDownCOCOTinyDataset'
+cfg.data.train.ann_file = f'{cfg.data_root}/annotations/train_baby_keypoints.json'
+cfg.data.train.img_prefix = f'{cfg.data_root}/images/train/'
+
+cfg.data.val.type = 'TopDownCOCOTinyDataset'
+cfg.data.val.ann_file = f'{cfg.data_root}/annotations/valid_baby_keypoints.json'
+cfg.data.val.img_prefix = f'{cfg.data_root}/images/valid/'
+
+cfg.data.test.type = 'TopDownCOCOTinyDataset'
+cfg.data.test.ann_file = f'{cfg.data_root}/annotations/test_baby_keypoints.json'
+cfg.data.test.img_prefix = f'{cfg.data_root}/images/test/'
