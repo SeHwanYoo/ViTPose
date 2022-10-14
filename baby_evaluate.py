3d0536d71985fb2706155ef55cfee58c1913c096
Sehwan Yoo <krstyle03v@gmail.com>
2022-10-14 15:20:35 +0900

    .

--- baby_evaluate.py
+++ baby_evaluate.py
@@ -1,78 +1,134 @@
-import mmcv
-from mmcv import Config
+# Copyright (c) OpenMMLab. All rights reserved.
+import os
+import warnings
+from argparse import ArgumentParser
+
+from xtcocotools.coco import COCO
 
 from mmpose.apis import (inference_top_down_pose_model, init_pose_model,
-                         vis_pose_result, process_mmdet_results)
-from mmdet.apis import inference_detector, init_detector
-import cv2
+                         vis_pose_result)
+from mmpose.datasets import DatasetInfo
 
-import sys 
-
-# sys.path.append('../')
-
-# local_runtime = False
-# try:
-#   from google.colab.patches import cv2_imshow  # for image visualization in colab
-# except:
-#   local_runtime = True
-
-cfg = Config.fromfile(
-    './configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/coco/ViTPose_base_coco_256x192.py'
-)
-
-# pose_config = 'configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/coco/ViTPose_base_coco_256x192.py'
-
-
-# pose_checkpoint = 'work_dirs/hrnet_w32_coco_tiny_256x192/latest.pth'
+pose_config = 'configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/coco/ViTPose_base_coco_256x192.py'
 pose_checkpoint = 'outputs/20221007/latest.pth'
-det_config = 'demo/mmdetection_cfg/faster_rcnn_r50_fpn_coco.py'
-det_checkpoint = 'https://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_1x_coco/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
 
-# initialize pose model
-pose_model = init_pose_model(cfg, pose_checkpoint)
-# initialize detector
-det_model = init_detector(det_config, det_checkpoint)
+json_file = '../../datasets/GM/annotations/test_baby_keypoints.json'
+img_root = '../../datasets/GM/images/test'
+out_img_root = 'evaluation' 
 
-# img = 'tests/data/coco/000000196141.jpg'
-img = '/home/sehwan/datasets/GM/images/test/004598.png'
+def main():
+    """Visualize the demo images.
 
-# inference detection
-mmdet_results = inference_detector(det_model, img)
+    Require the json_file containing boxes.
+    """
+    # parser = ArgumentParser()
+    # parser.add_argument('pose_config', help='Config file for detection')
+    # parser.add_argument('pose_checkpoint', help='Checkpoint file')
+    # parser.add_argument('--img-root', type=str, default='', help='Image root')
+    # parser.add_argument(
+    #     '--json-file',
+    #     type=str,
+    #     default='',
+    #     help='Json file containing image info.')
+    # parser.add_argument(
+    #     '--show',
+    #     action='store_true',
+    #     default=False,
+    #     help='whether to show img')
+    # parser.add_argument(
+    #     '--out-img-root',
+    #     type=str,
+    #     default='',
+    #     help='Root of the output img file. '
+    #     'Default not saving the visualization images.')
+    # parser.add_argument(
+    #     '--device', default='cuda:0', help='Device used for inference')
+    # parser.add_argument(
+    #     '--kpt-thr', type=float, default=0.3, help='Keypoint score threshold')
+    # parser.add_argument(
+    #     '--radius',
+    #     type=int,
+    #     default=4,
+    #     help='Keypoint radius for visualization')
+    # parser.add_argument(
+    #     '--thickness',
+    #     type=int,
+    #     default=1,
+    #     help='Link thickness for visualization')
 
-# extract person (COCO_ID=1) bounding boxes from the detection results
-person_results = process_mmdet_results(mmdet_results, cat_id=1)
+    # args = parser.parse_args()
+    # assert args.show or (args.out_img_root != '')
 
-# inference pose
-pose_results, returned_outputs = inference_top_down_pose_model(pose_model,
-                                                               img,
-                                                               person_results,
-                                                               bbox_thr=0.3,
-                                                               format='xyxy',
-                                                               dataset='TopDownCocoDataset')
+    coco = COCO(json_file)
+    # build the pose model from a config file and a checkpoint file
+    pose_model = init_pose_model(
+        pose_config, pose_checkpoint, device='cuda:1')
 
-# show pose estimation results
-vis_result = vis_pose_result(pose_model,
-                             img,
-                             pose_results,
-                             kpt_score_thr=0.,
-                             dataset='TopDownCocoDataset',
-                             show=False)
+    dataset = pose_model.cfg.data['test']['type']
+    dataset_info = pose_model.cfg.data['test'].get('dataset_info', None)
+    if dataset_info is None:
+        warnings.warn(
+            'Please set `dataset_info` in the config.'
+            'Check https://github.com/open-mmlab/mmpose/pull/663 for details.',
+            DeprecationWarning)
+    else:
+        dataset_info = DatasetInfo(dataset_info)
 
-# reduce image size
-vis_result = cv2.resize(vis_result, dsize=None, fx=0.5, fy=0.5)
+    img_keys = list(coco.imgs.keys())
+
+    # optional
+    return_heatmap = False
+
+    # e.g. use ('backbone', ) to return backbone feature
+    output_layer_names = None
+
+    # process each image
+    for i in range(len(img_keys)):
+        # get bounding box annotations
+        image_id = img_keys[i]
+        image = coco.loadImgs(image_id)[0]
+        image_name = os.path.join(img_root, image['file_name'])
+        ann_ids = coco.getAnnIds(image_id)
+
+        # make person bounding boxes
+        person_results = []
+        for ann_id in ann_ids:
+            person = {}
+            ann = coco.anns[ann_id]
+            # bbox format is 'xywh'
+            person['bbox'] = ann['bbox']
+            person_results.append(person)
+
+        # test a single image, with a list of bboxes
+        pose_results, returned_outputs = inference_top_down_pose_model(
+            pose_model,
+            image_name,
+            person_results,
+            bbox_thr=None,
+            format='xywh',
+            dataset=dataset,
+            dataset_info=dataset_info,
+            return_heatmap=return_heatmap,
+            outputs=output_layer_names)
+
+        if out_img_root == '':
+            out_file = None
+        else:
+            os.makedirs(out_img_root, exist_ok=True)
+            out_file = os.path.join(out_img_root, f'vis_{i}.jpg')
+
+        vis_pose_result(
+            pose_model,
+            image_name,
+            pose_results,
+            dataset=dataset,
+            dataset_info=dataset_info,
+            kpt_score_thr=0,
+            # radius=args.radius,
+            # thickness=args.thickness,
+            show=False, 
+            out_file=out_file)
 
 
-cv2.imwrite('outputs/20221007/images/result.png', vis_result)
-
-
-# if local_runtime:
-#   from IPython.display import Image, display
-#   import tempfile
-#   import os.path as osp
-#   import cv2
-#   with tempfile.TemporaryDirectory() as tmpdir:
-#     file_name = osp.join(tmpdir, 'pose_results.png')
-#     cv2.imwrite(file_name, vis_result)
-#     display(Image(file_name))
-# else:
-#   cv2_imshow(vis_result)
+if __name__ == '__main__':
+    main()
